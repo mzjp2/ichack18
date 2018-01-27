1bd39afac12997297c6534c32088396006713107
Zain Patel <30357972+mzjp2@users.noreply.github.com>
2018-01-27 15:21:02 +0000

    Zain organise

---
+++ test_stuff/jsonreader.py
@@ -0,0 +1,18 @@
+import json
+
+# usage
+# import jsonreader.py
+# jsonreader.spotify_client_id  will be the string for your client id
+#  
+
+
+
+with open('api_keys.json', 'r') as f:
+	data = json.load(f)
+
+
+spotify_client_id = data['spotify']['client_id']
+spotify_client_key = data['spotify']['client_key']
+
+mscv_key =  data['microsoft']['api_key']
+mslang_key = data['microsoft']['lang_key']
