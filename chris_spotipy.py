1abdc64f92000b26e4322d1693346891d688c834
Christopher Smith <misterchristophersmith@gmail.com>
2018-01-27 15:19:23 +0000

    Adding my files

--- chris_spotipy.py
+++ chris_spotipy.py
@@ -1,6 +1,17 @@
 import spotipy
-sp = spotipy.Spotify()
+import jsonreader
+from spotipy.oauth2 import SpotifyClientCredentials
 
-results = sp.search(q='weezer', limit=20)
-for i, t in enumerate(results['tracks']['items']):
-    print(' ', i, t['name'])
+client_credentials_manager = SpotifyClientCredentials(client_id=jsonreader.spotify_client_id, client_secret=jsonreader.spotify_client_key)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+despacito = '5CtI0qwDJkDQGwXD1H1cLb'
+
+
+uri = ''
+anal = sp.audio_features('5CtI0qwDJkDQGwXD1H1cLb')
+#print(anal)
+
+#print(sp.recommendation_genre_seeds())
+
+print(sp.recommendations(seed_tracks=['5CtI0qwDJkDQGwXD1H1cLb']))
