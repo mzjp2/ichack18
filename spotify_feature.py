1e8a0dd4c6a1d4fac56106e32e551c562206cb7a
Zain Patel <30357972+mzjp2@users.noreply.github.com>
2018-01-27 19:57:24 +0000

    updating

--- spotify_feature.py
+++ spotify_feature.py
@@ -1,8 +1,21 @@
 import spotipy, jsonreader, json
 from spotipy.oauth2 import SpotifyClientCredentials
 
+# authentication stuff
+
 client_credentials_manager = SpotifyClientCredentials(client_id=jsonreader.spotify_client_id, client_secret=jsonreader.spotify_client_key)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# generate recommended list based on genre
+
+def recommendedList(genre):
+	track_list = recommendations(seed_genres = genre, limit = 100)
+	return track_list
+
+
+
+
+# takes in track_list (list of tracks) and outputs json features of the track
 
 def dumpFeatures(track_list):
 
