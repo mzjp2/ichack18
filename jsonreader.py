7b136e7984726373f618acdba64d6a89d8067a7d
Christopher Smith <misterchristophersmith@gmail.com>
2018-01-27 14:24:43 +0000

    Removed print function from jsonreader.py

--- jsonreader.py
+++ jsonreader.py
@@ -11,7 +11,6 @@ with open('api_keys.json', 'r') as f:
 	#json.dump(data, f)
 	data = json.load(f)
 
-print(data['spotify'])
 
 spotify_client_id = data['spotify']['client_id']
 spotify_client_key = data['spotify']['client_key']
