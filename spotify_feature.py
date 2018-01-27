7e33a988b805b31159dff457bab005106622870c
Zain Patel <30357972+mzjp2@users.noreply.github.com>
2018-01-27 22:52:07 +0000

    updating spotify

--- spotify_feature.py
+++ spotify_feature.py
@@ -16,22 +16,29 @@ def generateGenre(spot_tags):
 # generate recommended list based on genre
 
 def recommendedList(genre):
-	track_list = sp.recommendations(seed_artists = None, seed_genres = genre, seed_tracks = None, limit = 100, country = None)
-	print(track_list)
+	recommendations_dict = sp.recommendations(seed_genres = [genre], limit = 100)
+	data_list = recommendations_dict['tracks']
+	track_list = [0] * 100
+	i=0
+	for d_point in data_list:
+		track_list[i] = d_point['id']
+		i += 1
+
+
 	return track_list
 # takes in track_list (list of tracks) and outputs json features of the track
 
 def dumpFeatures(track_list):
 
+	features = [0] * 100
+	i = 0
 	for track_id in track_list:
-
-		features = sp.audio_features(track_id)
-		track_name = sp.track(track_id)['name']
-		with open('features/' + track_name + '_features.json', 'w') as outfile:
-			json.dump(features, outfile, indent=4)
-
-	return None
+		features[i] = sp.audio_features(track_id)
+		i += 1
+	#	track_name = sp.track(track_id)['name']
+	return features
 
 genre = generateGenre(spot_tags)
 track_list = recommendedList(genre)
-dumpFeatures(track_list)
+feature = dumpFeatures(track_list)
+print(feature)
