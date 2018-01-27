2715b92210161f7a635daf41bdccddf7efb1d4d3
Christopher Smith <misterchristophersmith@gmail.com>
2018-01-27 14:18:39 +0000

    Updating jsonreader.py

--- jsonreader.py
+++ jsonreader.py
@@ -1,5 +1,10 @@
 import json
 
+# usage
+# import jsonreader.py
+# jsonreader.spotify_client_id  will be the string for your client id
+#  
+
 
 
 with open('api_keys.json', 'r') as f:
@@ -11,4 +16,4 @@ print(data['spotify'])
 spotify_client_id = data['spotify']['client_id']
 spotify_client_key = data['spotify']['client_key']
 
-mscv_key =  
+mscv_key =  data['microsoft']['api_key']
