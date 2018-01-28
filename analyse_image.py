adbb8d9ea14ae406ee01316006470854d6ebcd7a
joerosenberg <joerosenberg2@gmail.com>
2018-01-28 06:36:21 +0000

    Added weight generating script - problems with spotipy

--- analyse_image.py
+++ analyse_image.py
@@ -31,17 +31,56 @@ def get_image_tags(image_path):
 		print("[Errno {0}] {1}".format(e.errno, e.strerror))
 		return ""
 
+	# If the API does not detect any tags/categories/colours it does not create
+	# the key, leading to an error when we try to read it.
+	# We make sure the key exists otherwise we get an error when processing the data.
+	for key in ['tags', 'categories', 'color']:
+		if key not in data: data[key] = {'dominantColors':[]}
+
 	# Make data nice and easily readable :)
 	# Because I am a nice person :)
-	nice_data = {"tags": [img_object['name'] for img_object in data['tags'] 
-						  if img_object['confidence'] >= CONF_THRESHOLD],
-				 "categories": [img_category['name'] for img_category in data['categories']
-				 			    if img_category['score'] >= SCORE_THRESHOLD],
-				 "colors": data['color']['dominantColors']}
+	
+	nice_data = {'tags': [], 'categories': [], 'color': []}
+	try: 
+		nice_data['tags'] = [img_object['name'] for img_object in data['tags'] 
+						  if img_object['confidence'] >= CONF_THRESHOLD]
+	except TypeError as e:
+		print("No tags given by API.")
+		nice_data['tags'] = []
 
+	try: 
+		nice_data['categories'] = [img_category['name'] for img_category in 
+			data['categories'] if img_category['score'] >= SCORE_THRESHOLD]
+	except TypeError as e:
+		print("No categories given by API.")
+		nice_data['categories'] = []
+
+	try:
+		nice_data['color'] = data['color']['dominantColors']
+	except TypeError as e:
+		print("No colors given by API.")
+		nice_data['color'] = []
+
+	print(nice_data['tags'])
 	return nice_data
 
 if __name__ == "__main__":
-	print(get_image_tags("/home/joe/sea_test.jpg")["tags"])
+	images = ['sea_test.jpg', 'beach.jpg', 'bedroom.jpeg', 'city_street.jpg',
+	'gym.jpg', 'kids_party.jpg', 'library.jpg', 'office.jpg', 'park.jpg', 'party.jpg',
+	'fancy_cafe.jpg', 'cafe.jpg', 'train_interior.jpg', 'queens_college.jpg', 'funeral.jpg',
+	'bar.jpg', 'living_room.jpeg', 'kitchen.jpg', 'field.jpg', 'lake.jpg', 'museum.jpg',
+	'gallery.jpg', 'forest.jpg', 'beach2.jpg', 'gathering.jpg', 'hackathon.jpg',
+	'skatepark.jpeg']
+	tags_nonunique = [get_image_tags('/home/joe/' + image_name)['tags']
+					  for image_name in images]
+	tags_flattened = []
+	for sublist in tags_nonunique:
+		for tag in sublist:
+			tags_flattened.append(tag)
+	tags = list(set(tags_flattened))
+	print(tags)
+	print(len(tags))
+
+
 
 
