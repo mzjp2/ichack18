a2fb45fed99347bd31ceef770734508d4c0ba7a7
Zain Patel <30357972+mzjp2@users.noreply.github.com>
2018-01-28 01:42:35 +0000

    updated test_spotify

--- test_stuff/test_spotipy.py
+++ test_stuff/test_spotipy.py
@@ -1,10 +1,8 @@
-import spotipy
-import jsonreader
-import sys
+import spotipy, jsonreader, json, sys
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
 
-scope = 'playlist-modify-private'
+scope = 'playlist-read-private playlist-modify-private playlist-read-collaborative'
 username = 'cl1zn6lqeumrukzr0il0j2j24'
 
 token = util.prompt_for_user_token(username, scope, 
@@ -15,6 +13,19 @@ token = util.prompt_for_user_token(username, scope,
 if token:
 	sp = spotipy.Spotify(auth = token)
 	sp.trace = False
-	playlists = sp.user_playlist_create(username, 'background', public=False)
 else:
 	print("Can't get token")
+
+def playlistCreate():
+	playlists = sp.user_playlist_create(username, 'Background Music', public=False)
+	results = sp.current_user_playlists()
+	for i, item in enumerate(results['items']):
+		if "{name}".format(name = item['name']) == 'Background Music':
+			created_playlist_id = "{id}".format(id=item['id'])
+	return created_playlist_id
+
+def playlistAdd(created_playlist_id, tracks_list):
+	user_playlist_add_tracks(username, created_playlist_id, tracks_list)
+	return None
+
+	
